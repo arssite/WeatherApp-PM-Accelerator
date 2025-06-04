cc49104d248f7bfc8bbbf330d67b87c6235c4473
Anmol R. Srivastava <91518669+arssite@users.noreply.github.com>
2025-06-04 21:59:27 +0530

    api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:8000'; // Update this to your backend URL
+//const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://tempdeploy-1.onrender.com/';
 
 export const submitWeather = async (weatherData) => {
   try {
